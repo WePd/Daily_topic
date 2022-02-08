1396de86e8efba489804961fe38199a4b9a795fc
wepd <weped_x@163.com>
2022-02-08 22:25:48 +0800

    update

--- Daily_Topic/Javascript/test.js
+++ Daily_Topic/Javascript/test.js
@@ -12,29 +12,47 @@
 // }
 // console.log(a(1, 1, 1))
 
-var min = Math.min()
-max = Math.max()
-console.log(min, max)
-console.log(min < max)
+// var min = Math.min()
+// max = Math.max()
+// console.log(min, max)
+// console.log(min < max)
 
-var a = 1
-;(function a() {
-  a = 2
-  console.log(a)
-})()
+// var a = 1
+// ;(function a() {
+//   a = 2
+//   console.log(a)
+// })()
 
 // for (let i = 0; i < 3; i++) {
 //   setTimeout(() => console.log(i), 1);
 // }
 
-for (var i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1)
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1)
+// }
+
+// const person = { name: "yideng" }
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`
+// }
+// console.log(sayHi.call(person, 5))
+// console.log(sayHi.bind(person, 5)())
+
+// let a = {},
+//   b = "0",
+//   c = 0
+// a[b] = "li"
+// a[c] = "q"
+// console.log(a[b])
+var a = 0,
+  b = 0
+function A(a) {
+  A = function (b) {
+    alert(a + b++)
+  }
+  alert(a)
 }
 
-const person = { name: "yideng" }
-
-function sayHi(age) {
-  return `${this.name} is ${age}`
-}
-console.log(sayHi.call(person, 5))
-console.log(sayHi.bind(person, 5)())
+A(1)
+A(2)
